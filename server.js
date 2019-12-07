ca608c0642cef1e70e0c8aa4b46d3ae01eda9863
ruthtech <leeruths@gmail.com>
2019-12-07 17:31:20 -0500

    Fix React component not being found by Express server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use(express.static(path.join(__dirname, 'client/build'))); 
 
-app.get('/', (req, res) => {  
+app.get('*', (req, res) => {  
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 })
 
