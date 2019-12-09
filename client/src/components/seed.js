47c38ce545e4737ca86c756dc0d28fb3ca6b91d2
ruthtech <leeruths@gmail.com>
2019-12-09 13:32:28 -0500

    Fix compile error preventing all React pages from loading

--- client/src/components/seed.js
+++ client/src/components/seed.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import axios from "axios";
 
 function Seed() {
-  console.log("Server called");
-  async function loadVoters() {
+  useEffect(() => {
+    console.log("Server called");
+    loadVoters();
+  }, []);
+
+  const loadVoters = async () => {
     let response = await axios.get(
       "https://randomuser.me/api/?results=5000&nat=CA"
     );
     console.log("Server called", response);
   }
+
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default Seed;
