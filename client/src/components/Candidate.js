9c8f4594ca09c3168f7e78b39e97c077071e650d
ruthtech <leeruths@gmail.com>
2019-12-09 13:28:14 -0500

    Add Candidate page, styles to Login page

--- client/src/components/Candidate.js
+++ client/src/components/Candidate.js
@@ -1,24 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
 
-function Candidate() {
+function Candidate(props) {
+    const [candidate, setCandidate] = useState([]);
+
+    useEffect(() => {
+        loadCandidateInformation(candidate.id);
+    }, []);
+
+    const loadCandidateInformation = (candidateId) => {
+        // In production, this will be a call to the server to get the details
+        // for a particular candidate
+        try {
+            const mockCandidate = { 
+                name: "Lisa M.", 
+                pictureURL: "/candidate-pc-photo.jpg", 
+                party: "Conservative Party of Canada", 
+                district: "W01", 
+                partyColour: "#244982", 
+                id:"2345", 
+                phone: "1-800-100-1000",
+                address: "1 Anywhere St",
+                email: "lisam@pc.ca",
+                twitter: "@lisam-pc",
+                website: "http://lisam.pc.ca",
+                party_website: "http://www.pc.ca"
+            };
+            setCandidate(mockCandidate);
+        }
+        catch( err ) {
+            console.log(err);
+            this.setCandidate({});
+        }
+    }
 
     return (
-        <div className="container">
+        <div className="container" style={{ backgroundColor: candidate.partyColour, color: '#ffffff' }}>
             <div className="row">
+                <div className="col text-center">
+                    <Image src={candidate.pictureURL} />
+                </div>
                 <div className="col">
-                    <h1>Canada Votes Online</h1>
+                    <h1>{candidate.name}</h1>
+                    <h5>{candidate.party}</h5>
+                    <h5>{candidate.district}</h5>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <label id="uuidLabel" for="uuid"></label>
-                    <input id="uuid" type="text" aria-describedby="uuidLabel"></input>
+                  <Card style={{ backgroundColor: candidate.partyColour, color: '#ffffff' }}>
+                    <Card.Body>
+                      <Card.Title>Contact</Card.Title>
+                      <Card.Text>Phone: {candidate.phone}</Card.Text>
+                      <Card.Text>Office Address: {candidate.address}</Card.Text>
+                      <Card.Text>Email: {candidate.email}</Card.Text>
+                      <Card.Text>Twitter: <Card.Link href={candidate.twitter}>{candidate.twitter}</Card.Link></Card.Text>
+                      <Card.Text>Website: <Card.Link href={candidate.website}>{candidate.website}</Card.Link></Card.Text>                      
+                    </Card.Body>
+                  </Card>
                 </div>
             </div>
             <div className="row">
-                <div className="col">
-                    <label id="passwordLabel" for="password"></label>
-                    <input id="password" type="password" aria-describedby="passwordLabel"></input>
+                <div className="col d-flex justify-content-center">
+                    {/* <Link to={candidate.party_website} /> */}
+                    <h2><a href={candidate.party_website}>Conservative Party of Canada</a></h2>
+                </div>
+            </div>
+            <div className="row p-3">
+                <div className="col d-flex justify-content-around">
+                    <Button variant="secondary">View Candidates</Button>
+                    <Button variant="secondary">Home</Button>
                 </div>
             </div>
         </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
 
     return (
-        <div className="container">
+        <div className="container" style={{ backgroundColor: "#C4C4C4" }}>
             <div className="row">
                 <div className="col">
                     <h1>Canada Votes Online</h1>
                 </div>
             </div>
             <div className="row">
-                <div className="col">
-                    <label id="uuidLabel" for="uuid"></label>
-                    <input id="uuid" type="text" aria-describedby="uuidLabel"></input>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <label id="passwordLabel" for="password"></label>
-                    <input id="password" type="password" aria-describedby="passwordLabel"></input>
+                <div className="col pb-3">
+                  <Form>
+                    <Form.Group controlId="formBasicUUID">
+                      <Form.Label className="font-weight-bold mb-0">UUID</Form.Label>
+                      <Form.Control type="text" placeholder="Enter uuid" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                      <Form.Label className="font-weight-bold mb-0">Password</Form.Label>
+                      <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="secondary" type="submit">
+                      Sign in
+                    </Button>
+                  </Form>
                 </div>
             </div>
         </div>
