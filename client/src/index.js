3fc9f67bba5019cb0e9a48b2072faae8e664c87a
ruthtech <leeruths@gmail.com>
2019-12-30 14:00:41 -0500

    Remove some create-react-app generated parts and replace with ones for this app. Also remove files that aren't used any more (e.g. sql files, image files, mockData.js).

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- db/seed.sql
+++
@@ -1,4 +0,0 @@
--- Has to be seeded with UUID and temporary passwords in order for user to log in for the first time.
--- Nice-to-have: force user to update temporary password after first login
-
-INSERT INTO users (name, address_line1, address_line2, postal_code, city, province, district, UUID, salt, password, hasVoted) VALUES ("Joe Smith", "Unit 802", "12 Main Street", "M1M 1M1", "Toronto", "ON", "E02", "0123456789", "d512dc906ded86ba323632e26858ef85", "b0ba031e04b9fcf34270cdd6280a3bc80f6d6cfe6a0b3e1e14f574ab8d740298ca33589f6459eba2c38e29d943794c5262d58f238ba4c08e30235760b627cb21");
