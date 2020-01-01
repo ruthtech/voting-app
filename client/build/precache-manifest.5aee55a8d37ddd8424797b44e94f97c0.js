94a05bf688de4f2d32a39af8b6924ab65c0b8571
ruthtech <leeruths@gmail.com>
2020-01-01 15:36:10 -0500

    Heroku can't find the build files. Thought it generated those itself but will add them back in.

---
+++ client/build/precache-manifest.5aee55a8d37ddd8424797b44e94f97c0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac2011aef475fffc31ae59d7345840d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7fc0a3c2e0fb1f98266",
+    "url": "/static/css/2.190b0f1a.chunk.css"
+  },
+  {
+    "revision": "c9ea09bc75de39a147c7",
+    "url": "/static/css/main.2142aa49.chunk.css"
+  },
+  {
+    "revision": "d7fc0a3c2e0fb1f98266",
+    "url": "/static/js/2.5e53b86d.chunk.js"
+  },
+  {
+    "revision": "f98b907d798ae66ae7674c30725da9bc",
+    "url": "/static/js/2.5e53b86d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9ea09bc75de39a147c7",
+    "url": "/static/js/main.af2b2f52.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "dea429e4f47c9ae95e13275794279836",
+    "url": "/static/media/canada-flag.dea429e4.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
