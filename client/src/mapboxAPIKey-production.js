c7c0376288d0c1ba565bcbd7091290c6b454795f
ruthtech <leeruths@gmail.com>
2020-01-05 10:40:51 -0500

    Remove unnecessary import

--- client/src/mapboxAPIKey-production.js
+++ client/src/mapboxAPIKey-production.js
@@ -1,3 +1,2 @@
-import mapboxgl from 'mapbox-gl';
 let productionMapToken = "pk.eyJ1IjoiZXNjaGVyZmFuIiwiYSI6ImNrNHl4bGszeDA1Z2MzZXM3enl3dXAwdGQifQ.GQLGYq0XDlXApqGgkKqF2A";
 export default productionMapToken;
