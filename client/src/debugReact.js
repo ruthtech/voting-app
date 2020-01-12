80abdb0e77850c31764d59d1d62f43edfdf14195
ruthtech <leeruths@gmail.com>
2020-01-11 20:55:04 -0500

    Remove unused fields from the database before updating the addresses to ones that exist.

--- client/src/debugReact.js
+++ client/src/debugReact.js
@@ -2,7 +2,6 @@ import ottawaCentreDistrictBoundaries from './components/assets/geoJSON/ottawaCe
 
 const hannahWhite = { 
     "_id" : "5df12740d7c15a6378d6b8b5", 
-    "gender" : "female", 
     "name" : {
         "title" : "Ms", 
         "first" : "Hannah", 
@@ -22,44 +21,14 @@ const hannahWhite = {
             "latitude" : 53.5533, 
             "longitude" : -113.4877
         }, 
-        "timezone" : {
-            "offset" : "-7:00", 
-            "description" : "Edmonton"
-        },
         "district": "Ottawa Centre",
         "districtBoundaries": ottawaCentreDistrictBoundaries
     }, 
-    "email" : "hannah.white@example.com", 
     "login" : {
-        "uuid" : "3047a2a1-6bda-42b3-b519-8ecd288406de", 
         "username" : "sadmouse166", 
         "password" : "circle", 
-        "salt" : "z14C7Za9", 
-        "md5" : "8fd37f57b68a0f6d735e025197446b6c", 
-        "sha1" : "1047eec0ccc8af1f0374837d7e048afd5f5085ab", 
-        "sha256" : "16a01c6c6cd33933b96d99b8b7c9c21878eae00fd176c82049cb9357fb5e291e"
     }, 
-    "dob" : {
-        "date" : "1981-09-18T07:14:21.069Z", 
-        "age" : 38
-    }, 
-    "registered" : {
-        "date" : "2007-12-09T02:42:39.014Z", 
-        "age" : 12
-    }, 
-    "phone" : "205-691-0535", 
-    "cell" : "581-225-3702", 
-    "id" : {
-        "name" : "", 
-        "value" : null
-    }, 
-    "picture" : {
-        "large" : "https://randomuser.me/api/portraits/women/57.jpg", 
-        "medium" : "https://randomuser.me/api/portraits/med/women/57.jpg", 
-        "thumbnail" : "https://randomuser.me/api/portraits/thumb/women/57.jpg"
-    }, 
-    "nat" : "CA", 
-    "hasvoted" : null
+    "hasvoted" : false
   };
 
 const ottawaCentreCandidatesList = [
--- models/Voter.js
+++ models/Voter.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema;
 const voterSchema = new Schema({
   _id: [Schema.Types.ObjectId],
   isAdmin: { type: Boolean, default: false },
-  gender: {
-    type: String,
-    trim: true
-  },
   name: {
     title: { type: String },
     first: { type: String },
@@ -26,42 +22,12 @@ const voterSchema = new Schema({
       latitude: { type: Number },
       longitude: { type: Number }
     },
-    timezone: {
-      offset: { type: String },
-      description: { type: String }
-    }
   },
-  email: { type: String },
   login: {
-    uuid: { type: String },
     username: { type: String },
     password: { type: String },
-    salt: { type: String },
-    md5: { type: String },
-    sha1: { type: String },
-    sha256: { type: String }
   },
-  dob: {
-    date: { type: Date },
-    age: { type: Number }
-  },
-  registered: {
-    date: { type: Date },
-    age: { type: Number }
-  },
-  phone: { type: String },
-  cell: { type: String },
-  idtype: {
-    name: { type: String },
-    value: { type: String }
-  },
-  picture: {
-    large: { type: String },
-    medium: { type: String },
-    thumbnail: { type: String }
-  },
-  nat: { type: String },
-  hasvoted: { type: String }
+  hasvoted: { type: Boolean }
 });
 
 let VoterModel = mongoose.model("VoterModel", voterSchema, "voter"); // model name, schema, collection name
